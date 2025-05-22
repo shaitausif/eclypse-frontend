bce3ecb16dad5071773e69e5c9c80bb945edfd47
Tausif Shaikh <141828254+shaitausif@users.noreply.github.com>
2025-05-23 01:54:12 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{
-      // This will append the http://localhost:3000 before api everywhere
-      '/api' : 'https://eclypse-backend.vercel.app/'
-      // It will also help us to get rid of that CORS error as now the vite can understand that both server and frontend request's are coming from the same port so it won't give any kind of error
-    }
-  },
+ 
   plugins: [
     tailwindcss(),
     react()],
