1de1e734cc23dc4222638a4ecf3a2f0719711fce
Tausif Shaikh <141828254+shaitausif@users.noreply.github.com>
2025-05-23 01:51:02 +0530

    Update DropBox.tsx

--- src/components/DropBox.tsx
+++ src/components/DropBox.tsx
@@ -1,48 +1,48 @@
-// import axios from "axios";
+import axios from "axios";
 import { ArrowDown, ArrowUp } from "lucide-react";
 import { useState } from "react";
-// import {useEffect} from 'react';
+import {useEffect} from 'react';
 
 const DropBox = () => {
   const [openIndex, setOpenIndex] = useState<number | null>(null);
-  // const [values, setvalues] = useState([{
-  //   subject : '',
-  //   description : ''
-  // }])
+  const [values, setvalues] = useState([{
+    subject : '',
+    description : ''
+  }])
 
-  const values = [
-    {
-      subject: "Size & Fit",
-      description:
-        "Getting the right fit is key! Our comprehensive size guide (link to size guide) provides detailed measurements and conversion charts to help you choose with confidence. Plus, each product page features specific fit notes from our team, letting you know if the item is designed for a relaxed, regular, or slim fit, and if it tends to run true to size, small, or large. Still unsure? Our customer service team is happy to help with personalized advice!",
-    },
-    {
-      subject: "Delivery & Returns",
-      description:
-        "Delivery: We offer several convenient shipping options to get your new favorites to you quickly. Choose from standard, express, or expedited delivery at checkout. You'll receive tracking information as soon as your order ships.\n\nReturns: Not quite right? No problem! We offer free returns on eligible items within 30 days of purchase. Simply initiate a return through our easy online portal, and we'll guide you through the process for a refund or exchange. For full details on our return policy and eligibility, please visit our Returns & Exchanges page (link to policy).",
-    },
-    {
-      subject: "How This Was Made",
-      description:
-        "Designed with passion and precision, this item showcases our dedication to quality craftsmanship. From the initial sketch to the final stitch, every detail was carefully considered. We work with skilled artisans who bring our visions to life, ensuring each piece is made to last and designed to be loved.",
-    },
-  ];
+  // const values = [
+  //   {
+  //     subject: "Size & Fit",
+  //     description:
+  //       "Getting the right fit is key! Our comprehensive size guide (link to size guide) provides detailed measurements and conversion charts to help you choose with confidence. Plus, each product page features specific fit notes from our team, letting you know if the item is designed for a relaxed, regular, or slim fit, and if it tends to run true to size, small, or large. Still unsure? Our customer service team is happy to help with personalized advice!",
+  //   },
+  //   {
+  //     subject: "Delivery & Returns",
+  //     description:
+  //       "Delivery: We offer several convenient shipping options to get your new favorites to you quickly. Choose from standard, express, or expedited delivery at checkout. You'll receive tracking information as soon as your order ships.\n\nReturns: Not quite right? No problem! We offer free returns on eligible items within 30 days of purchase. Simply initiate a return through our easy online portal, and we'll guide you through the process for a refund or exchange. For full details on our return policy and eligibility, please visit our Returns & Exchanges page (link to policy).",
+  //   },
+  //   {
+  //     subject: "How This Was Made",
+  //     description:
+  //       "Designed with passion and precision, this item showcases our dedication to quality craftsmanship. From the initial sketch to the final stitch, every detail was carefully considered. We work with skilled artisans who bring our visions to life, ensuring each piece is made to last and designed to be loved.",
+  //   },
+  // ];
 
    // Due to some errors while deploying my express server on vercel I am unable to fetch the data from the server right now, due to time constraint I am submitting this project to you and I am also trying to solve that error and if it got resolved i will make the changes in the application also
   
-  // useEffect(() => {
-  //   const fetchTextData = async() => {
-  //     try {
-  //       const response = await axios.get('/api/v1/text-data')
-  //       setvalues(response.data)
+  useEffect(() => {
+    const fetchTextData = async() => {
+      try {
+        const response = await axios.get('/api/v1/text-data')
+        setvalues(response.data)
       
-  //     } catch (error) {
-  //       console.log("Error while fetching the data")
-  //       throw new Error("Error while fetching the data")
-  //     }
-  //   }
-  //     fetchTextData();
-  // },[])
+      } catch (error) {
+        console.log("Error while fetching the data")
+        throw new Error("Error while fetching the data")
+      }
+    }
+      fetchTextData();
+  },[])
 
 
 
